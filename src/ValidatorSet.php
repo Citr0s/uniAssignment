4150ad744207c8649ee5cd6283b0733331808a9a
Milosz Dura <miloszdura@gmail.com>
2015-12-10 09:22:24 +0000

    validatorset addItem wrapper added

--- src/ValidatorSet.php
+++ src/ValidatorSet.php
@@ -4,6 +4,11 @@ namespace Assignment;
 class ValidatorSet extends Collection
 {
 	private $errors = array();
+
+	public function addItem(Validator $item, $key = null){
+		parent::addItem($item, $key);
+	}
+
 	public function getErrors(){
 		foreach($this->members as $key => $value){
 			if(!is_null($value->getError())){
